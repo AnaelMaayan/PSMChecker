c825d309284f1e354715d3ab15244f6b735369a9
AnaelMaayan <anaelmaayan@gmail.com>
2024-05-12 13:11:03 +0300

    Bug fixes

--- PSMChecker.ps1
+++ PSMChecker.ps1
@@ -39,7 +39,7 @@ function ComponentsFolder {
         return $true
     }
     else {
-        retrun $false
+        return $false
     }   
 }
 
@@ -652,8 +652,8 @@ else {
     write-host ""
     Write-Host "Connected with Local Administrator:$IsAdmin"  -ForegroundColor Yellow
 }
-    
-if ($IsAdmin -and (ComponentsFolder -eq $true)) {
+$componentsFolderCheck = ComponentsFolder
+if ($IsAdmin -and ($componentsFolderCheck -eq $true)) {
     $stepsCounter = 0
     write-host ""
     if ($DOMAIN_ACCOUNTS) {
@@ -753,7 +753,7 @@ else {
             Write-Host "Need to be connected with local Administrator" -ForegroundColor Red    
         }
     }
-    if (ComponentsFolder -eq $false)
+    if ($componentsFolderCheck -eq $false)
     {
         Write-Host "The script must run on PSM server." -ForegroundColor Red
     }
